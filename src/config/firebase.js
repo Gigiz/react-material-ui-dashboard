719d1f89a39188e8246ddbdb89fbf167c418fc05
Luigi Colombi <gigizstudio@gmail.com>
2019-09-11 17:38:18 +0200

    add firebase config for production build (same as development)

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -5,7 +5,15 @@ import 'firebase/firestore';
 import 'firebase/messaging';
 
 const firebaseConfig = {
-  production: {},
+  production: {
+    apiKey: 'AIzaSyBB5Nvgpl-YZOXW5lupTAcQE52gfiafO38',
+    authDomain: 'family-assistant-dev.firebaseapp.com',
+    databaseURL: 'https://family-assistant-dev.firebaseio.com',
+    projectId: 'family-assistant-dev',
+    storageBucket: 'family-assistant-dev.appspot.com',
+    messagingSenderId: '928402288765',
+    appId: '1:928402288765:web:e871e804e8b26a34158114',
+  },
   development: {
     apiKey: 'AIzaSyBB5Nvgpl-YZOXW5lupTAcQE52gfiafO38',
     authDomain: 'family-assistant-dev.firebaseapp.com',
