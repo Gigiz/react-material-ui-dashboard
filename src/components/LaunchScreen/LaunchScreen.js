3a19fd420beff252b1c3ab7ba65b8b894897f914
Luigi Colombi <gigizstudio@gmail.com>
2019-09-11 10:34:56 +0200

    remove propTypes

--- src/components/LaunchScreen/LaunchScreen.js
+++ src/components/LaunchScreen/LaunchScreen.js
@@ -1,15 +1,10 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import CircularProgress from '@material-ui/core/CircularProgress';
 
 const LaunchScreen = () => {
   const classes = useStyles();
   return <CircularProgress className={classes.circularProgress} />;
-};
-
-LaunchScreen.propTypes = {
-  classes: PropTypes.object.isRequired
 };
 
 export default LaunchScreen;
