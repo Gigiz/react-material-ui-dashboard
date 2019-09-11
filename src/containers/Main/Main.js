5750a94640b45abc97c1070de30335f8a2fa02c8
Luigi Colombi <gigizstudio@gmail.com>
2019-09-11 10:23:33 +0200

    lazy import for routes

--- src/containers/Main/Main.js
+++ src/containers/Main/Main.js
@@ -1,27 +1,31 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import PrivateRoute from '@src/components/PrivateRoute/PrivateRoute';
-import WelcomePage from '@src/containers/pages/WelcomePage/WelcomePage';
-import LoginPage from '@src/containers/pages/LoginPage/LoginPage';
-import DashboardPage from '@src/containers/pages/DashboardPage/DashboardPage';
+import LaunchScreen from '@src/components/LaunchScreen/LaunchScreen';
 
-const Main = () => <Switch>
-  <Route
-    exact
-    path='/'
-    component={WelcomePage}
-  />
-  <Route
-    path='/login'
-    component={LoginPage}
-  />
-  <PrivateRoute
-    path='/dashboard'
-    component={DashboardPage}
-  />
-  <Route
-    render={() => <div>404</div>}
-  />
-</Switch>;
+const LoginPage = lazy(() => import('@src/containers/pages/LoginPage/LoginPage'));
+const WelcomePage = lazy(() => import('@src/containers/pages/WelcomePage/WelcomePage'));
+const DashboardPage = lazy(() => import('@src/containers/pages/DashboardPage/DashboardPage'));
+
+const Main = () => <Suspense fallback={<LaunchScreen />}>
+  <Switch>
+    <Route
+      exact
+      path='/'
+      component={WelcomePage}
+    />
+    <Route
+      path='/login'
+      component={LoginPage}
+    />
+    <PrivateRoute
+      path='/dashboard'
+      component={DashboardPage}
+    />
+    <Route
+      render={() => <div>404</div>}
+    />
+  </Switch>
+</Suspense>;
 
 export default Main;
