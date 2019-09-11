aa0bd4a8116c34217d55802b52cf9e27f43ee711
Luigi Colombi <gigizstudio@gmail.com>
2019-09-11 10:23:48 +0200

    remove unused styles

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,31 +1,16 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import { makeStyles } from '@material-ui/core/styles';
 import Main from '@src/containers/Main/Main';
 import TopMenu from '@src/components/TopMenu/TopMenu';
 import UserContextProvider from '@src/models/context/UserContext/UserContext';
 
-const App = () => {
-  const classes = useStyles();
-  
-  return <BrowserRouter>
-    <UserContextProvider>
-      <CssBaseline />
-      <TopMenu />
-      <div className={classes.root}>
-        <Main />
-      </div>
-    </UserContextProvider>
-  </BrowserRouter>
-};
+const App = () => <BrowserRouter>
+  <UserContextProvider>
+    <CssBaseline />
+    <TopMenu />
+    <Main />
+  </UserContextProvider>
+</BrowserRouter>;
 
 export default App;
-
-function useStyles() {
-  return makeStyles(theme => ({
-    root: {
-      margin: theme.spacing(1),
-    },
-  }))();
-}
